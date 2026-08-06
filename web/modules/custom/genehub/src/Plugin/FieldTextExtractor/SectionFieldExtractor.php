ba9e5f55e54a3187c8c380599866fb6426a260c7
James Wang <stjuan627@gmail.com>
2026-08-06 17:57:57 +0800

    fix(ai-translate): translate GeneHub product fields

---
+++ web/modules/custom/genehub/src/Plugin/FieldTextExtractor/SectionFieldExtractor.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\genehub\Plugin\FieldTextExtractor;
+
+use Drupal\ai_translate\Attribute\FieldTextExtractor;
+use Drupal\ai_translate\Plugin\FieldTextExtractor\FieldExtractorBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Extracts translatable properties from GeneHub section fields.
+ */
+#[FieldTextExtractor(
+  id: 'genehub_section',
+  label: new TranslatableMarkup('GeneHub section'),
+  field_types: ['genehub_section'],
+)]
+final class SectionFieldExtractor extends FieldExtractorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getColumns(): array {
+    return ['heading', 'body'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setValue(ContentEntityInterface $entity, string $fieldName, array $textMeta): void {
+    $newValue = $entity->get($fieldName)->getValue();
+
+    foreach ($textMeta as $delta => $singleValue) {
+      unset(
+        $singleValue['field_name'],
+        $singleValue['field_type'],
+        $singleValue['_columns'],
+      );
+
+      $newValue[$delta] = isset($newValue[$delta])
+        ? array_merge($newValue[$delta], $singleValue)
+        : $singleValue;
+    }
+
+    $entity->set($fieldName, $newValue);
+  }
+
+}
